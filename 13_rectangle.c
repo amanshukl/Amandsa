83704e2486e38388bbf4dd1aea32243efffad389
amanshukl <amanshuk916@gmail.com>
2024-11-12 12:54:43 +0530

    handshake problem

---
+++ 13_rectangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+int r ,largestrectangle;
+printf("enter the radius:");
+scanf("%d",&r);
+largestrectangle=(2*(r*r));
+printf("area of largest rectangle is:%d",largestrectangle);
+    return 0;
+}
